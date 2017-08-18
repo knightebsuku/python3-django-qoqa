6d6dd443191c9ec9e0a4a5998fca2f61e3b99cfc
lunga <lunga@localhost>
2017-08-18 17:15:42 +0200

    dev

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='python3-qoqa',
+    version='0.1.0',
+    packages=find_packages(),
+    scripts=['qoqa.py'],
+    package_data={
+        'qoqa': [
+            'data/debian/*.example',
+            'data/MANIFEST.in.example',
+            'data/setup.py.example',
+            'data/template.zip'
+        ]
+
+    },
+    author="Lunga Mthembu",
+    author_email="stumenz.complex@gmail.com",
+    description="Commandline application to package django projects",
+    license="GPLv3",
+    keywords="python3 django virtualenv",
+    url="https://github.com/ebsuku/python3-qoqa",
+)
