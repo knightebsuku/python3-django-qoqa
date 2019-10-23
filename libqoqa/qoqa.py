a18be6d1ff70ddea9a3a45312b53316319912ee3
lunga <stumenz.complex@gmail.com>
2019-10-23 16:35:11 +0200

    code refactoring and clean up

--- libqoqa/qoqa.py
+++ libqoqa/qoqa.py
@@ -7,7 +7,7 @@ import re
 from colorama import Fore
 
 from . import virtualenv
-from . import database as db
+from . import database
 from . import build
 
 INIT_PROJECT_CONFIG = {}
@@ -17,139 +17,144 @@ def django_version():
     """
     Specify a django version
     """
-    dict_version = {
-        "1": "1.11.*",
-        "2": "2.0.*"
-    }
-    print(Fore.GREEN + 'Avaliable django versions')
+    dict_version = {"1": "1.11.*", "2": "2.2.*"}
+    print(Fore.GREEN + "Avaliable django versions")
     print(Fore.GREEN + "1 - 1.11 LTS")
-    print(Fore.GREEN + "2 - 2.0")
-    version = input(Fore.GREEN + "[qoqa] select django version number [1]: ")
-    if version == '':
-        return dict_version['1']
-    elif version not in ['1', '2']:
-        print(Fore.RED + 'Invalid selection, options are 1 or 2')
-        django_version()
-    return dict_version[version]
+    print(Fore.GREEN + "2 - 2.2 LTS")
+    while True:
+        version = input(Fore.GREEN + "[qoqa] select django version number [1]: ")
+        if version == "":
+            return dict_version["1"]
+        elif version not in ["1", "2"]:
+            print(Fore.RED + "Invalid selection, options are 1 or 2")
+        return dict_version[version]
 
 
 def generate_key():
     """
     Generate Secret key for django
     """
-    key = ''.join([random.SystemRandom().choice(string.ascii_letters +
-                                                string.digits +
-                                                string.punctuation)
-                   for _ in range(50)])
+    key = "".join(
+        [
+            random.SystemRandom().choice(
+                string.ascii_letters + string.digits + string.punctuation
+            )
+            for _ in range(50)
+        ]
+    )
     return key
 
 
-def production_config(project_name: str):
+def create_production_settings():
     """
-    Create production.cfg file
+    Ask user if they want to setup production configurations
     """
-    print(Fore.GREEN + "[qoqa] Creating production.cfg file")
+    options = {"yes": True, "no": False}
+    while True:
+        status = input(
+            Fore.GREEN + "Do you wan to create production configuration files? [yes]: "
+        )
+        if status not in ["yes", "no"]:
+            print(Fore.RED + "Invalid option, please type yes or no")
+        else:
+            return options[status]
+
+
+def configuration_file(status, db_config):
+    """
+    Create a configuration file based on the status(production, development)
+    """
     config = configparser.RawConfigParser()
-    config['STATUS'] = {
-        'Production': True
-    }
-    config['SECRET_KEY'] = {
-        'value': ''.join(generate_key())
-    }
-    db_conf = INIT_PROJECT_CONFIG['PROD_DB']
-    config['PRODUCTION_DATABASE'] = {k: v for k, v in db_conf.items()}
-    config['ALLOWED_HOSTS'] = {'Hosts': '127.0.0.1,localhost'}
-    with open('production.cfg', 'w') as production_file:
-        config.write(production_file)
-    print(Fore.GREEN + "[qoqa] Configuration file for production created")
+    print(Fore.GREEN + f"[qoqa] Creating {status} configuration file")
+
+    config["SECRET_KEY"] = {"value": "".join(generate_key())}
+    config["ALLOWED_HOSTS"] = {"Hosts": "127.0.0.1,localhost"}
+    config["DATABASE"] = db_config
+
+    if status == "development":
+        config["STATUS"] = {"Production": False}
+
+    elif status == "production":
+        config["STATUS"] = {"Production": True}
+    else:
+        print(Fore.RED + "Unknown development status")
+        exit()
+    with open(f"{status}.cfg", "w") as status_file:
+        config.write(status_file)
+    print(Fore.GREEN + f"[qoqa] {status} configuration file created")
 
 
-def development_config(project_name: str):
-    """
-    Create configuration file for django project
-    """
-    print(Fore.GREEN + "[qoqa] Creating configuration file")
-    config = configparser.RawConfigParser()
-    config['STATUS'] = {
-        'Production': False
-    }
-    config['SECRET_KEY'] = {
-        'value': ''.join(generate_key())
-    }
-    dev = INIT_PROJECT_CONFIG['DEV_DB']
-    config['DEVELOPMENT_DATABASE'] = {k: v for k, v in dev.items()}
-    config['ALLOWED_HOSTS'] = {'Hosts': '127.0.0.1,localhost'}
-    with open(project_name+'.cfg', 'w') as project_file:
-        config.write(project_file)
-    print(Fore.GREEN + '[qoqa] Configuration file has been created')
-
-
-def create(project_name: str):
+def create(project_name):
     """
     Launch interactive console and setup new project
     """
-    if project_name in ['new', 'build', 'release']:
-        print(Fore.RED + '[qoqa] invalid project name: {}'.format(project_name))
+    if project_name in ["new", "build", "release"]:
+        print(Fore.RED + "[qoqa] invalid project name: {}".format(project_name))
         exit()
     if os.path.isdir(project_name):
         print(Fore.RED + "[qoqa] project directory already exists")
         exit()
-    print(Fore.BLUE + "[qoqa] Configuring New Django Project")
+    print(Fore.GREEN + "[qoqa] Configuring New Django Project")
     version = django_version()
-    prod_db = db.setup()
+    prod_status = create_production_settings()
     os.mkdir(project_name)
     os.chdir(project_name)
-    virtualenv.create(project_name, version, prod_db)
-    development_config(project_name)
-    production_config(project_name)
-    print(Fore.GREEN + "[qoqa] Project {} has been setup".format(project_name))
+    db_config = database.create("development")
+    configuration_file("development", db_config)
+    if prod_status:
+        db_config = database.create("production")
+        configuration_file("production", db_config)
+
+    # virtualenv.create(project_name, version, prod_status)
+
+    print(Fore.GREEN + f"[qoqa] Project {project_name} has been setup")
 
 
 def new_build(version: str):
     """
     Update project version and check that all required files are present
     """
-    if not re.match('[\d.]+', version):
-        print(Fore.RED + '[qoqa] incorrect version format')
+    if not re.match("[\d.]+", version):
+        print(Fore.RED + "[qoqa] incorrect version format")
         exit()
 
     project_directory = os.path.basename(os.getcwd())
     config = configparser.ConfigParser()
-    config['DJANGO_PROJECT_VERSION'] = {'Version': version}
-    with open('qoqa.cfg', 'w') as cfg:
+    config["DJANGO_PROJECT_VERSION"] = {"Version": version}
+    with open("qoqa.cfg", "w") as cfg:
         config.write(cfg)
     print(Fore.GREEN + "[qoqa] Checking that all requirements are met.")
-    if os.path.isfile('setup.py'):
+    if os.path.isfile("setup.py"):
         print(Fore.GREEN + "[qoqa] setup.py file exists")
     else:
         print(Fore.GREEN + "[qoqa] setup.py file does not exist, creating.")
         build.python_setup_file()
 
-    if os.path.isfile('MANIFEST.in'):
-        print(Fore.GREEN + '[qoqa] MANIFEST.in file exists')
+    if os.path.isfile("MANIFEST.in"):
+        print(Fore.GREEN + "[qoqa] MANIFEST.in file exists")
     else:
         print(Fore.GREEN + "[qoqa] MANIFEST.in has not been created.")
         build.manifest()
 
-    if os.path.isfile(os.path.join(project_directory, 'start_gunicorn')):
-        print(Fore.GREEN + '[qoqa] start_gunicorn file exists')
+    if os.path.isfile(os.path.join(project_directory, "start_gunicorn")):
+        print(Fore.GREEN + "[qoqa] start_gunicorn file exists")
     else:
-        print(Fore.GREEN + '[qoqa] Creating new start_gunicorn script')
+        print(Fore.GREEN + "[qoqa] Creating new start_gunicorn script")
         build.gunicorn_file()
 
-    if os.path.isfile(os.path.join(project_directory, '__init__.py')):
-        print(Fore.GREEN + '[qoqa] project __init__.py file exists')
+    if os.path.isfile(os.path.join(project_directory, "__init__.py")):
+        print(Fore.GREEN + "[qoqa] project __init__.py file exists")
     else:
         build.init_file()
-        print(Fore.GREEN + '[qoqa]  __init__.py file created')
+        print(Fore.GREEN + "[qoqa]  __init__.py file created")
 
-    if os.path.isfile('requirements.txt'):
+    if os.path.isfile("requirements.txt"):
         print(Fore.GREEN + "[qoqa] requirements.txt file exists")
     else:
         print(Fore.GREEN + "[qoqa] requirements.txt has not been created")
         build.requirements()
 
-    if os.path.isdir('debian'):
+    if os.path.isdir("debian"):
         print(Fore.GREEN + "[qoqa] debian directory exists, updating version")
         build.update_changelog(version)
     else:
@@ -165,15 +170,15 @@ def release(version):
     """
     Create .deb package.
     """
-    if not re.match('[\d.]+', version):
+    if not re.match("[\d.]+", version):
         print(Fore.RED + "[qoqa] Incorrect version format")
         exit()
     config = configparser.ConfigParser()
-    config.read('qoqa.cfg')
-    if version != config['DJANGO_PROJECT_VERSION']['Version']:
+    config.read("qoqa.cfg")
+    if version != config["DJANGO_PROJECT_VERSION"]["Version"]:
         print(Fore.RED + "[qoqa] Build version and release version to not match")
         exit()
-    print(Fore.GREEN + '[qoqa] Creating .deb for django project')
+    print(Fore.GREEN + "[qoqa] Creating .deb for django project")
     build.dpkg(version)
 
 
--- libqoqa/virtualenv.py
+++ libqoqa/virtualenv.py
@@ -8,13 +8,14 @@ from .build import DATA_DIRECTORY
 from colorama import Fore
 
 
-template_zip = os.path.join(DATA_DIRECTORY, 'template.zip')
+template_zip = os.path.join(DATA_DIRECTORY, "template.zip")
 
 
 class SingleVenv(venv.EnvBuilder):
     """
     Class to just create a virtualenv on an existing project
     """
+
     def __init__(self, dj_version, *args, **kwargs):
         self.dj_version = dj_version
         super().__init__(*args, **kwargs)
@@ -23,29 +24,26 @@ class SingleVenv(venv.EnvBuilder):
         """
         install default applications
         """
-        os.environ['VIRTUAL_ENV'] = context.env_dir
-        pip = os.path.join(context.bin_path, 'pip')
+        os.environ["VIRTUAL_ENV"] = context.env_dir
+        pip = os.path.join(context.bin_path, "pip")
         try:
             print(Fore.GREEN + "[qoqa] Installing pip files ")
             print(Fore.GREEN + "[qoqa] Preparing to install django")
-            subprocess.run([pip, 'install',
-                            'django=={}'.format(self.dj_version)],
-                           check=True)
+            subprocess.run(
+                [pip, "install", "django=={}".format(self.dj_version)], check=True
+            )
             print(Fore.GREEN + "[qoqa] django package installed")
-            print(Fore.GREEN + '[qoqa] Preparing to install whitenoise')
-            subprocess.run([pip, 'install', 'whitenoise'], check=True)
+            print(Fore.GREEN + "[qoqa] Preparing to install whitenoise")
+            subprocess.run([pip, "install", "whitenoise"], check=True)
             print(Fore.GREEN + "[qoqa] whitenoise package installed")
-            print(Fore.GREEN + "[qoqa] Preparing to "
-                  "install django-debug-toolbar")
-            subprocess.run([pip, 'install', 'django-debug-toolbar'],
-                           check=True)
+            print(Fore.GREEN + "[qoqa] Preparing to " "install django-debug-toolbar")
+            subprocess.run([pip, "install", "django-debug-toolbar"], check=True)
             print(Fore.GREEN + "[qoqa] django-debug-toolbar package installed")
             print(Fore.GREEN + "[qoqa] Preparing into install gunicorn")
-            subprocess.run([pip, 'install', 'gunicorn'], check=True)
+            subprocess.run([pip, "install", "gunicorn"], check=True)
             print(Fore.GREEN + "[qoqa] gunicorn package installed")
         except subprocess.CalledProcessError as err:
-            print(Fore.RED + "[qoqa] Unable to download pip files,"
-                  "cleaning up")
+            print(Fore.RED + "[qoqa] Unable to download pip files," "cleaning up")
             print(Fore.RED + "[qoqa] removing incomplete directory")
             os.chdir("..")
             shutil.rmtree(self._project_name)
@@ -57,56 +55,55 @@ class SingleVenv(venv.EnvBuilder):
         """
         check for requirements file and pull dependencies if needed
         """
-        pip = os.path.join(context.bin_path, 'pip')
-        if os.path.isfile('requirements.txt'):
+        pip = os.path.join(context.bin_path, "pip")
+        if os.path.isfile("requirements.txt"):
             try:
                 print(Fore.GREEN + "[qoqa] fetching dependencies")
-                subprocess.run([pip, 'install', '-r', 'requirements.txt'],
-                               check=True)
+                subprocess.run([pip, "install", "-r", "requirements.txt"], check=True)
             except subprocess.CalledProcessError as error:
                 print(Fore.RED + "[qoqa] Unable to download pip files")
                 exit()
         else:
-            print(Fore.BLUE + "[qoqa] Unable to find requirements.txt "
-                  "Continuing.....")
+            print(
+                Fore.BLUE + "[qoqa] Unable to find requirements.txt " "Continuing....."
+            )
 
 
 class ExtendVenv(venv.EnvBuilder):
-
-    def __init__(self, project_name, *args, **kwargs):
+    def __init__(
+        self, project_name, django_version, production_status, *args, **kwargs
+    ):
         self._project_name = project_name
-        self._dj_version = args[0]
-        self.prod_db = args[1]
+        self._django_version = django_version
+        self.prod_status = production_status
         super().__init__(*args, **kwargs)
 
     def post_setup(self, context):
         """
         install default applications
         """
-        os.environ['VIRTUAL_ENV'] = context.env_dir
-        print("The path is {}".format(context.bin_path))
-        pip = os.path.join(context.bin_path, 'pip')
+        os.environ["VIRTUAL_ENV"] = context.env_dir
+        print("The path is {context.bin_path}")
+        pip = os.path.join(context.bin_path, "pip")
         try:
             print(Fore.GREEN + "[qoqa] Installing pip files: ")
             print(Fore.GREEN + "[qoqa] Preparing to install django")
-            subprocess.run([pip,
-                            'install',
-                            'django=={}'.format(self._dj_version)],
-                           check=True)
-            print(Fore.GREEN + '[qoqa] Preparing to install whitenoise')
-            subprocess.run([pip, 'install', 'whitenoise'], check=True)
+            subprocess.run(
+                [pip, "install", "django=={self._django_version"], check=True
+            )
+            print(Fore.GREEN + "[qoqa] Preparing to install whitenoise")
+            subprocess.run([pip, "install", "whitenoise"], check=True)
             print(Fore.GREEN + "[qoqa] installing django-debug-toolbar")
-            subprocess.run([pip, 'install', 'django-debug-toolbar'],
-                           check=True)
+            subprocess.run([pip, "install", "django-debug-toolbar"], check=True)
             print(Fore.GREEN + "[qoqa] Preparing into install gunicorn")
-            subprocess.run([pip, 'install', 'gunicorn'], check=True)
-            if self.prod_db == '2':
+            subprocess.run([pip, "install", "gunicorn"], check=True)
+            if self.prod_status:
                 print(Fore.GREEN + "[qoqa] installing psycopg2")
-                subprocess.run([pip, 'install', 'psycopg2'],
-                               check=True)
-        except subprocess.CalledProcessError as err:
-            print(Fore.RED + "[qoqa] Unable to download pip files "
-                  "cleaning up")
+                subprocess.run([pip, "install", "psycopg2"], check=True)
+        except subprocess.CalledProcessError as error:
+            print(
+                Fore.RED + f"[qoqa] Unable to download pip files cleaning up: {error}"
+            )
             exit()
         else:
             self._startproject(context)
@@ -115,23 +112,21 @@ class ExtendVenv(venv.EnvBuilder):
         """
         Create a new django project
         """
-        dj_admin_script = os.path.join(context.bin_path, 'django-admin')
+        dj_admin_script = os.path.join(context.bin_path, "django-admin")
         print(Fore.GREEN + "[qoqa] initializing django project")
         try:
-            subprocess.run([
-                dj_admin_script,
-                'startproject',
-                '--template='+template_zip,
-                self._project_name], check=True)
+            subprocess.run(
+                [dj_admin_script, "startproject", "--template=" + template_zip, "."],
+                check=True,
+            )
 
-            print(Fore.GREEN + "[qoqa] django project directory created")
+            print(Fore.GREEN + "[qoqa] django project created")
             os.chdir(self._project_name)
         except subprocess.CalledProcessError as error:
-            print(Fore.RED + "Unable to use zip template")
-            print(error)
+            print(Fore.RED + f"Unable to use zip template: {error}")
             exit()
         else:
-            os.chmod('manage.py', stat.S_IRWXU)
+            os.chmod("manage.py", stat.S_IRWXU)
 
 
 def create(project_name: str, dj_version: str, prod_db: str):
@@ -139,4 +134,4 @@ def create(project_name: str, dj_version: str, prod_db: str):
     create new virtual environment
     """
     env = ExtendVenv(project_name, dj_version, prod_db, with_pip=True)
-    env.create('env3')
+    env.create("env3")
