eb780f784c2365a0478ec8b1faf263424764de71
ebsuku <stumenz.complex@gmail.com>
2020-09-06 13:30:47 +0200

    new version

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python3-qoqa',
-    version='0.9.2',
+    name="python3-django-qoqa",
+    version="1.0.0",
     packages=find_packages(),
-    scripts=['qoqa'],
+    scripts=["django-qoqa.py"],
     package_data={
-        'libqoqa': [
-            'data/debian/*.example',
-            'data/MANIFEST.in.example',
-            'data/setup.py.example',
-            'data/start_gunicorn.example',
-            'data/template.zip'
+        "qoqa": [
+            "data/debian/*.example",
+            "data/MANIFEST.in.example",
+            "data/setup.py.example",
+            "data/start_gunicorn.example",
+            "data/template.zip",
         ]
-
     },
     author="Lunga Mthembu",
     author_email="stumenz.complex@gmail.com",
     description="Commandline application to package django projects",
     license="GPLv3",
     keywords="python3 django virtualenv",
-    url="https://github.com/ebsuku/python3-qoqa",
+    url="https://github.com/ebsuku/python3-django-qoqa",
 )
